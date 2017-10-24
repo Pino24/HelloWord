a199c6310dbb0df7243811e5c3e976416c1781e9
yanggen@aorise.cn <yanggen@aorise.cn>
2017-10-24 15:36:40 +0800

    Initial commit

---
+++ src/com/aorise/test/Mytest.java
@@ -0,0 +1,10 @@
+package com.aorise.test;
+
+/**
+ * Created by Administrator on 2017/10/24.
+ */
+public class Mytest {
+    public static void main(String[] args) {
+        System.out.println("Hello word!");
+    }
+}
